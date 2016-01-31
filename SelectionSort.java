716ac45275b77c1efb0c1aa51476c7791db8a6a2
Pushpankar Kumar Pushp <pushpankark@gmail.com>
2016-01-31 09:05:31 +0530

    Implement Selection Sorting

---
+++ SelectionSort.java
@@ -0,0 +1,18 @@
+public class SelectionSort{
+	public void sort(int[] unsortedArr){
+		//Create an instance of Test class to take input from user
+		Test test=new Test();
+		for(int i=0;i<unsortedArr.length;i++){
+			//This loop iterates over all the elements from 0
+			int min=i;
+			for(int j=i+1;j<unsortedArr.length;j++){
+				//Here i am finding the lowest element in the remaining unsorted loop 
+				if (unsortedArr[min]>unsortedArr[j]) {
+					min=j;
+				}
+			}
+			//Swaping the lowest element from the first element, thus increaing the size of the sorted array by 1.
+			test.swap(unsortedArr,min,i);
+		}
+	}
+}
